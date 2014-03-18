fa9fdc072af76b5303b69e7f7c896e2791b48ecc
portalsway2 <portalsway2@gmail.com>
2014-03-18 10:54:38 +0100

    ddd

---
+++ app/PhpStorm-133.679/bin/inspect.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# ------------------------------------------------------
+# PhpStorm offline inspection script.
+# ------------------------------------------------------
+#
+
+export DEFAULT_PROJECT_PATH="$(pwd)"
+
+IDE_BIN_HOME="${0%/*}"
+exec "$IDE_BIN_HOME/phpstorm.sh" inspect "$@"
